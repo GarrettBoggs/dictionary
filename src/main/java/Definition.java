a6c40580dcca800721d90dc1e4287eb7ca2eb9c8
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-09-16 08:22:13 -0700

    add description and first test for description

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Definition {
-  
+  private String mInformation;
+  private static List<Definition> instances = new ArrayList<Definition>();
+  private int mId;
+
+  public Definition(String info){
+    mInformation = info;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+  public String getInformation() {
+   return mInformation;
+  }
+
+ public static List<Definition> all() {
+   return instances;
+ }
+
+ public static void clear() {
+   instances.clear();
+ }
+
+ public int getId() {
+   return mId;
+ }
+
+ public static Definition find(int id) {
+ return instances.get(id - 1);
+ }
+
 }
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -1,5 +1,13 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
+
 public class DefinitionTest {
+
+  @Test
+  public void defintion_instantiatesCorrectly_true() {
+    Definition testCategory = new Definition("to move quickly");
+    assertEquals(true, testCategory instanceof Definition);
+  }
+
 }
