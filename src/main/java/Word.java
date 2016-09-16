49d09daca4beef8d6887c54f625aa4c3fd9d91e0
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-09-16 08:42:17 -0700

    create word class and tests

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,3 +1,45 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Word {
-  
+  private String mSpelling;
+  private static List<Word> instances = new ArrayList<Word>();
+  private int mId;
+  private List<Definition> mDefinitions;
+
+  public Word(String spelling)
+  {
+    mSpelling = spelling;
+    instances.add(this);
+    mId = instances.size();
+    mDefinitions = new ArrayList<Definition>();
+  }
+
+    public String getSpelling() {
+     return mSpelling;
+   }
+
+   public static List<Word> all() {
+    return instances;
+   }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+   public int getId() {
+     return mId;
+   }
+
+   public static Word find(int id) {
+    return instances.get(id - 1);
+  }
+
+   public List<Definition> getDefinitions() {
+     return mDefinitions;
+   }
+
+   public void addDefinition(Definition task) {
+     mDefinitions.add(task);
+   }
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -1,5 +1,62 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.time.LocalDateTime;
+
 public class WordTest {
+
+    @Test
+  public void category_instantiatesCorrectly_true() {
+    Word testWord = new Word("run");
+    assertEquals(true, testWord instanceof Word);
+  }
+
+  @Test
+  public void getName_categoryInstantiatesWithSpelling_run() {
+    Word testWord = new Word("run");
+    assertEquals("run", testWord.getSpelling());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfWord_true() {
+    Word firstWord = new Word("run");
+    Word secondWord = new Word("sprint");
+    assertEquals(true, Word.all().contains(firstWord));
+    assertEquals(true, Word.all().contains(secondWord));
+  }
+
+  @Test
+  public void clear_emptiesAllWordsFromList_0() {
+   Word testWord = new Word("run");
+   Word.clear();
+   assertEquals(Word.all().size(), 0);
+  }
+
+  @Test
+  public void getId_wordInstantiateWithAnId_1() {
+   Word.clear();
+   Word testWord = new Word("run");
+   assertEquals(1, testWord.getId());
+  }
+
+  @Test
+  public void find_returnsWordWithSameId_secondWord() {
+   Word.clear();
+   Word firstWord = new Word("run");
+   Word secondWord = new Word("sprint");
+   assertEquals(Word.find(secondWord.getId()), secondWord);
+  }
+
+  @Test
+  public void getDefinitions_initiallyReturnsEmptyList_ArrayList() {
+    Word.clear();
+    Word testWord = new Word("run");
+    assertEquals(0, testWord.getDefinitions().size());
+  }
+
+  @Test
+  public void addDefinition_addsDefinitionToList_true() {
+    Word testWord = new Word("run");
+    Definition testDefinition = new Definition("move at a speed faster than a walk");
+    testWord.addDefinition(testDefinition);
+    assertTrue(testWord.getDefinitions().contains(testDefinition));
+  }
 }
